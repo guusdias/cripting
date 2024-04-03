ef17398287d23a64e5298138e200b45c2e4de5c8
Cristian Prochnow <cristianprochnow@gmail.com>
2024-04-02 23:02:12 -0300

    improve code of SHA

--- SHA.py
+++ SHA.py
@@ -1,16 +1,55 @@
 import hashlib
 
-h = hashlib.new("sha256")
-correct_password = "MyPSWS32I3"
-h.update(correct_password.encode())
+class Encrytor:
+    def __init__(self, password):
+        self.__hash = None
+        self.__password = password
 
-password_hash = h.hexdigest()
-print("Password Hash:", password_hash)
+    def __correct_password(self):
+        return "gelo"
 
-user_input = "MYPASSKAS2332"
-h = hashlib.new("sha256")
-h.update(user_input.encode())
-input_hash = h.hexdigest()
+    @staticmethod
+    def tip():
+        return "É feito de água, mas se for colocado dentro da água morrerá."
 
-print("Input Hash:", input_hash)
-print("Passwords Match:", input_hash == password_hash)
+    def __new(self):
+        self.__hash = hashlib.new("sha256")
+
+    def __encode(self, value):
+        self.__hash.update(value.encode())
+
+    def __get(self):
+        return self.__hash.hexdigest()
+
+    def start(self):
+        data = {}
+
+        data["initial_correct_password"] = self.__correct_password()
+        self.__new()
+        self.__encode(self.__correct_password())
+        data["correct_password"] = self.__get()
+
+        data["initial_inserted_password"] = self.__password
+        self.__new()
+        self.__encode(self.__password)
+        data["inserted_password"] = self.__get()
+
+        data["matches"] = data["correct_password"] == data["inserted_password"]
+
+        return data
+
+print("A resposta da charada a seguir é a senha!")
+print("\n")
+print(Encrytor.tip())
+print("\n")
+password = input("Senha: ")
+
+encryption = Encrytor(password)
+encryptionResult = encryption.start()
+
+print("Senha Inserida Inicial: "+encryptionResult["initial_inserted_password"])
+print("Senha Correta Inicial: "+encryptionResult["initial_correct_password"])
+print("Senha Inserida com SHA: "+encryptionResult["inserted_password"])
+print("Senha Correta com SHA: "+encryptionResult["correct_password"])
+print("\n")
+print("Senhas Batem: "+("Sim" if encryptionResult["matches"] else "Não")+"!")
